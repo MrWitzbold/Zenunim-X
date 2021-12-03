0767ed9da066295cb12f45917f3ccc5fa07e6052
JUKjacker <80601981+MrWitzbold@users.noreply.github.com>
2021-12-02 22:05:29 -0300

    Untested Prototype of training is done

--- main.py
+++ main.py
@@ -42,11 +42,11 @@ class text_neural_network():
         
         if first_time_executing:
             for i in range(0, 820): # i tested it, it's correct
-                neurons.append(0)
+                neurons.append(0.1)
                 weights.append([])
             for i in range(0, len(weights)):
                 for i in range(0, 820):
-                    weights[i].append(0)
+                    weights[i].append(0.1)
         else:
             neurons_aux = neurons_file.replace(" ", "").replace("[", "").replace("]", "").split(",")
             for i in range(0, len(neurons_aux)):
@@ -86,22 +86,67 @@ class text_neural_network():
                     return True
 
         def train(self, inputs, outputs, iterations):
+            neurons = self.neurons
+            weights = self.weights
+            neuron_layers = self.neuron_layers
+
             def sigmoid(number):
                 result = number/(math.sqrt(1 + number**2))
                 return result
             
             def unsigmoid(x):
                 print("Getting value for: " + str(abs(x)))
-                result = abs(math.sqrt((0-x^2)/(x^2 - 1)))
+                result = abs(math.sqrt((0-x**2)/(x**2 - 1)))
                 return result
 
             for i in range(0, iterations):
                 print("Iteration: " + str(i))
-            
+
                 # The values from the inputs to the first 40 neurons, we're gonna pass panually
                 # For the values of the next neurons, we'll use a function that says which neurons
                 # are connected to the current neuron
                 # and a function to get the connections' respective weights from the matrix
+
+                for j in range(0, len(inputs)):
+                    input_ = inputs[j]
+                    output = outputs[j]
+    
+                    # First we'll pass the input values to the first 40 neurons
+    
+                    for neuron_index in range(0, len(neurons[0])):
+                        neurons[0][neuron_index] = sigmoid(ord(input_[(neuron_index) % len(input_)])) # loop inside the input
+                        
+                    print("Initial neurons: " + str(neurons[0]))
+    
+                    for layer in range(1, len(neuron_layers)): # remember that the neuron layers simply contain the indexes of the neurons
+                        for neuron_index in range(0, len(neuron_layers[layer])):
+                            neuron_value = 0
+                            for neuron_index_2 in range(0, len(neuron_layers[layer-1])):
+                                neuron_value += neurons[layer-1][neuron_index_2] * weights[neuron_index][neuron_index_2]
+                            neurons[layer][neuron_index] = neuron_value
+                    
+                    print("Trained neurons: " + str(neurons))
+    
+                    # Feed forward is over, now we're on the backpropagation
+    
+                    # First we'll calculate the error of the output layer
+    
+                    neural_output = neurons[len(neurons)]
+    
+                    error = ((neural_output - sigmoid(output))**2)/2
+
+                    learning_rate = 0.0001
+
+                    for line in range(0, len(weights)):
+                        for column in range(0, len(weights[line])):
+                            weights[line][column] -= weights[line][column] - learning_rate*(error/weights[line][column])
+                    
+                    save_state()
+
+
+                
+
+
 
 
 
